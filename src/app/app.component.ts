bda3eebd96903338aa70c8f1f45cf384123d276b
Francisco <francisco.serrano372@gmail.com>
2019-02-09 17:56:28 -0300

    Adds skeleton for user input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'errepar-webapp-demo';
+  title = 'Errepar - Demo Recomendador';
 
-  listaTipoPersonas = ['PF', 'PJ', 'ND'];
-  tipoPersonaSeleccionado: string;
+  listaTipoPersona = ['Física', 'Jurídica', 'N/D'];
+  listaTiposDebitoAutomatico = ['SI', 'NO', 'N/D'];
+  listaCantidadSuscripciones = Array.from(Array(14).keys());
+  listaSuscripciones = [
+    'COL ERREPAR CONSULTOR TRIBUTARIO', 'ERREIUS ON LINE',
+    'COL ERREPAR SANTA FE EOL (10)', 'ERREIUS ON LINE (DEMO)',
+    'COL ERREPAR CONSULTOR TRIBUTARIO', 'ENC. LAB. CLASICA PAPEL- 5 T. + DP',
+    'ENC. LAB. CLASICA PAPEL- 5 T.', 'COL ERREPAR RURAL',
+    'ENC. TRIB. CLASICA EOL (3)', 'ERREIUS (20)'
+  ];
+  listaDomicilios = [
+    'CIUDAD AUTONOMA BUENOS AIRES', 'CORDOBA', 'ROSARIO', 'LA PLATA', 'SANTA FE',
+    'MAR DEL PLATA', 'SALTA', 'MENDOZA', 'SAN MIGUEL DE TUCUMAN', 'BAHIA BLANCA',
+    'N/D'
+  ];
+
+  // Variables input del formulario
+  tipoPersonaSeleccionado = undefined;
+  edadIngresada = undefined;
+  tasaEOLIngresada = undefined;
+  tasaIUSIngresada = undefined;
+  tipoDebitoAutomaticoSeleccionado = undefined;
+  scoreIngresado = undefined;
+
+  suscripcion = [];
+
+  domicilioIngresadoA = undefined;
+  domicilioIngresadoB = undefined;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MatToolbarModule, MatCardModule, MatIconModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatToolbarModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -18,12 +23,13 @@ import { MatInputModule } from '@angular/material/input';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatCardModule,
     MatIconModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
